943a55f2524f8452f7753406cb0263f06042d2b2
niladhikari <niladhikari445@gmail.com>
2024-02-18 23:28:55 +0600

    add the some components and add the message box and add the chat box

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import {Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-
-
+import Login from "./Pages/Login/Login";
+import SignUp from './Pages/SignUp/SignUp';
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-     {/* <Login></Login> */}
-     {/* <SignUp></SignUp> */}
-     <Home></Home>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home/>}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="/signup"
+          element={<SignUp />}
+        />
+      </Routes>
     </div>
   );
 }
